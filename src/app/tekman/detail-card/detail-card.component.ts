a3b0876f9be192dd8c24f1713ff9a13f62fc13c6
Juan <ingjef@gmail.com>
2022-08-02 18:17:07 +0200

    Add style in table result card

--- src/app/tekman/detail-card/detail-card.component.ts
+++ src/app/tekman/detail-card/detail-card.component.ts
@@ -7,22 +7,21 @@ import { CardService } from '../services/card.service';
 
 export interface PeriodicElement {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  position: string;
+  img: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: '1', name: 'Ordinales', img: 'tekmanEyes.png'},
+  {position: '2', name: 'Ordinales y fracciones', img: 'tekmanEyes.png'},
+  {position: '3', name: 'Fracciones y decimales', img: 'tekmanEyes.png'},
+  {position: '4', name: 'Ordinales, Fracciones y Decimales', img: 'tekmanEyes.png'},
+  {position: '5', name: 'Ordinales, Fracciones, Decimales y Geometría',  img: 'tekmanEyes.png'},
+  {position: '6', name: 'Carbon',  img: 'tekmanEyes.png'},
+  {position: '7', name: 'Geometría y Fracciones',  img: 'tekmanEyes.png'},
+  {position: '8', name: 'Fracciones, Decimales y  Geometría', img: 'tekmanEyes.png'},
+  {position: '9', name: 'Ordinales, Fracciones, Decimales,Geometria, Números', img: 'tekmanEyes.png'},
+  {position: '10', name: 'Fracciones', img: 'tekmanEyes.png'},
 ];
 @Component({
   selector: 'app-detail-card',
@@ -31,7 +30,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class DetailCardComponent implements OnInit {
 
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns = ['position', 'name', 'img'];
   dataSource = ELEMENT_DATA;
   clickedRows = new Set<PeriodicElement>();
   idCard: any;
